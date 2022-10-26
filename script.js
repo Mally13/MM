7abab4597126ac49d9111915443a3f44db0a2a38
Mally13 <mallymutuku13@gmail.com>
2022-10-26 12:43:25 +0300

    new

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
 const readMoreBtn = document.querySelector(".read-more-btn");
 const text = document.querySelector(".text");
 const mycards=document.querySelector(".mycards");
 
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
 
 readMoreBtn.addEventListener("click", function(){
   text.classList.toggle("show-more");
@@ -12,3 +28,4 @@ readMoreBtn.addEventListener("click", function(){
     readMoreBtn.innerHTML= "Read More";
   }
 });
+
